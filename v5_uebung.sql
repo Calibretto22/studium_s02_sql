b5a111f5d80caa2416b8d812c9506d3519c3868a
Calibretto <github.com@sentinelx.de>
2018-01-09 20:23:56 +0100

    aufgaben

--- v5_uebung.sql
+++ v5_uebung.sql
@@ -1,7 +1,11 @@
+# vorlesung5 übungsblatt 1
+
+# aufgabe 1: ordnen sie die marke der fahrzeuge absteigend nach dem beim verkauf erzielten gewinn
 select autonr, marke, vpreis-epreis gewinn 
 from auto 
 order by gewinn DESC;
 
+# aufgabe 2: geben sie die marke der fahrzeuge aus, die maja oder willy besessen haben
 select distinct auto.marke, auto.autonr, vorbesitzer.name 
 from auto, vorbesitzer, gehoerte
 where vorbesitzer.name = "Maja" 
@@ -9,14 +13,18 @@ or vorbesitzer.name = "Willy"
 and vorbesitzer.vbnr = gehoerte.vbnr 
 and gehoerte.autonr = auto.autonr;
 
+# aufgabe 3: geben sie die marke der fahrzeuge und die posten der wartung der fahrzeuge aus, gruppiert nach der marke der fahrzeuge
 select auto.marke, wartung.posten 
 from auto, ist_in, wartung 
 where auto.autonr = ist_in.autonr
 and ist_in.wartnr = wartung.wartnr
 group by auto.marke; 
 
+# aufgabe 4: für wie viel geld wurden bereits autos eingekauft 
 select SUM(epreis) from auto;
 
+# vorlesung5 übungsblatt 1
+# aufgabe 1: geben sie absteigend sortiert, die vorbesitzer des fahrzeugs an, dessen bremmsscheiben gewechselt wurden
 select distinct auto.marke, gehoerte.vbnr, vorbesitzer.name 
 from wartung, vorbesitzer, auto, ist_in, gehoerte 
 where wartung.posten = "Bremsscheiben" 
